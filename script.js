e1e42b4c2a0ab9f1cc9501799269e0ff66549f5b
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2021-07-12 18:31:57 -0300

    commit ate quase feito

--- script.js
+++ script.js
@@ -4,3 +4,47 @@ let td = document.querySelectorAll('td');
 for (let index in td) {
     td[index].className = 'pixel';
 }
+
+//troca selected via click
+function addSelected() {
+let color = document.querySelectorAll('.color');
+
+for (let index = 0; index < color.length; index += 1) {
+    function addSelected(event) {
+        if (color[index].classList.contains('selected')) {
+            color[index].classList.remove('selected');
+        } else {
+            event.target.classList.add('selected');
+        }
+    }
+
+    let li = document.querySelectorAll('li');
+    for (let secondIndex in li) {
+        if (li[secondIndex].click) {
+            li[secondIndex].addEventListener('click', addSelected);
+        }
+    }
+}
+}
+addSelected();
+
+//Troca aplica a cor selecionada
+function aplicaColor() {
+    let color = document.querySelectorAll('li');
+
+    for (let index = 0; index < color.length; index += 1) {
+        function addSelected(event) {
+            if (color[index].classList.contains('selected')) {
+                event.target.style.backgroundColor = 'black';
+            }
+        }
+
+        let td = document.querySelectorAll('td');
+        for (let secondIndex in td) {
+            if (td[secondIndex].click) {
+                td[secondIndex].addEventListener('click', addSelected);
+            }
+        }
+    }
+}
+aplicaColor();
